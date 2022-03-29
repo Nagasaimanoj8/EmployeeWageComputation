9bb22afaf6d7e8ee97d4808b3d1fa75488ddb0c1
Nagasaimanoj8 <nagasaimanoj333@gmail.com>
2022-03-29 10:09:18 +0530

    WagesForMonth

--- WageCompute.cs
+++ WageCompute.cs
@@ -10,29 +10,37 @@ namespace EmployeeWageCompute
     {
         static void Main(string[] args)
         {
-            const int isFullTime = 1;
-            const int isPartTime = 2;
-            int empRatePerHr = 20;
+            const int Full_Time = 1;
+            const int Part_Time = 2;
+            const int MAX_WORKING_DAYS = 20;
+            const int EMP_RATE_PER_HR = 20;
             int empHrs = 0;
             int empWage = 0;
+            int totalWage = 0;
             Random random = new Random();
-            int empInput = random.Next(0, 3);
-            switch (empInput)
+            for (int day = 1; day <= MAX_WORKING_DAYS; day++)
             {
-                case isFullTime:
-                    Console.WriteLine("employee is present");
-                    empHrs = 8;
-                    break;
-                case isPartTime:
-                    Console.WriteLine("employee is present");
-                    empHrs = 4;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
+
+                int empInput = random.Next(0, 3);
+                switch (empInput)
+                {
+                    case Full_Time:
+                        Console.WriteLine("employee is present");
+                        empHrs = 8;
+                        break;
+                    case Part_Time:
+                        Console.WriteLine("employee is present");
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = EMP_RATE_PER_HR * empHrs;
+                Console.WriteLine("Daily Wage :{0} for day{1}", empWage, day);
+                totalWage += empWage;
             }
-            empWage = empRatePerHr * empHrs;
-            Console.WriteLine("Daily Wage :{0}", empWage);
+            Console.WriteLine("Total Wage {0} for {1} days", totalWage, MAX_WORKING_DAYS);
             Console.ReadLine();
         }
 
