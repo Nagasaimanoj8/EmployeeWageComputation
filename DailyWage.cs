d15fd11f347a9477e8702203347e30a3869fbdd0
Nagasaimanoj8 <nagasaimanoj333@gmail.com>
2022-03-29 09:24:32 +0530

    Wage

---
+++ DailyWage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyEmployeeWage
+{
+    internal class DailyWage
+    {
+        static void Main(string[] args)
+        {
+            int isPresent = 1;
+            int empRatePerHr = 20;
+            int empHrs = 0;
+            int empWage = 0;
+            Random random = new Random();
+            int empInput=random.Next(0,2);
+            if(isPresent == empInput)
+            {
+                Console.WriteLine("employee is present");
+                empHrs = 8;
+
+            }
+            else
+            {
+                Console.WriteLine("employee absent");
+            }
+            empWage = empRatePerHr * empHrs;
+            Console.WriteLine("Daily Wage:{0}",empWage);
+            Console.ReadLine();
+        }
+    }
+}
