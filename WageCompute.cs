733010e4470fb0c123896296ab5569f9c492c465
Nagasaimanoj8 <nagasaimanoj333@gmail.com>
2022-03-29 09:50:05 +0530

    Attend

---
+++ WageCompute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageCompute
+{
+    internal class WageCompute
+    {
+        static void Main(string[] args)
+        {
+            int isPresent = 1;
+            Random random = new Random();
+            int empInput = random.Next(0, 2);
+
+            if (isPresent == empInput)
+            {
+                Console.WriteLine("employee present");
+
+            }
+            else
+            {
+                Console.WriteLine("employee absent");
+            }
+        }
+    }
+}
