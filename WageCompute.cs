a8e8513df4786cea5271549cad01e24ef49a79e5
Nagasaimanoj8 <nagasaimanoj333@gmail.com>
2022-03-29 09:53:30 +0530

    DailyWage

--- WageCompute.cs
+++ WageCompute.cs
@@ -11,18 +11,24 @@ namespace EmployeeWageCompute
         static void Main(string[] args)
         {
             int isPresent = 1;
+            int empRatePerHr = 20;
+            int empHrs = 0;
+            int empWage = 0;
             Random random = new Random();
             int empInput = random.Next(0, 2);
-
             if (isPresent == empInput)
             {
-                Console.WriteLine("employee present");
+                Console.WriteLine("employee is present");
+                empHrs = 8;
 
             }
             else
             {
                 Console.WriteLine("employee absent");
             }
+            empWage = empRatePerHr * empHrs;
+            Console.WriteLine("Daily Wage:{0}", empWage);
+            Console.ReadLine();
         }
     }
 }
