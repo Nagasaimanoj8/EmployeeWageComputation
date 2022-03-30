e271f6fbd437295220b53d90fe8a445a15688f57
Nagasaimanoj8 <nagasaimanoj333@gmail.com>
2022-03-30 08:55:04 +0530

    Welcome

--- WageCompute.cs
+++ WageCompute.cs
@@ -8,55 +8,10 @@ namespace EmployeeWageCompute
 {
     internal class WageCompute
     {
-        public const int FULL_TIME = 1;     //Constant variables
-        public const int PART_TIME = 2;
-        public const int EMP_RATE_PER_HOUR = 20;
-        public const int NUM_OF_WORKING_DAYS = 20;
-        public const int MAX_WORKING_HRS = 100;
-        public const int MAX_WORKING_DAYS = 20;
-        public static int ComputeEmployeeWage()
+        static void Main(string[] args)
         {
-            int emphrs = 0;
-            int empWage = 0;
-            int totalempwage = 0;
-            int hrs = 0;
-            int workingDays = 1;
-            Random random = new Random();       //Random Class
-            while (hrs < MAX_WORKING_HRS && workingDays <= MAX_WORKING_DAYS)
-
-            {
-
-                workingDays++;
-                int EmpCheack = random.Next(0, 3);      //Random Generate 0 ,1,2
-                switch (EmpCheack)          //Switch case Statment
-                {
-                    case FULL_TIME:         //Employee is FullTime=1
-                        emphrs = 8;
-                        break;
-                    case PART_TIME:          //Employee is FullTime=2
-                        emphrs = 4;
-                        break;
-                    default:
-                        emphrs = 0;
-                        break;
-                }
-
-                empWage = EMP_RATE_PER_HOUR * emphrs;            // Calculate empWage
-                hrs += emphrs;                                           //Display empwage
-                totalempwage = totalempwage + empWage;      //Calculate total employe month wage
-            }
-            Console.WriteLine("Employe Wage Per Day :- " + empWage);
-            Console.WriteLine("Total Employe Month Wage :- " + totalempwage);
-            Console.WriteLine("Employee wage for " + workingDays + " days " + totalempwage);
-            Console.WriteLine("Working hours " + hrs);
-            return totalempwage;
+            Console.WriteLine("Welcome to Employee Wage Computation");
 
         }
-        public static void Main(string[] args)
-        {
-            WageCompute.ComputeEmployeeWage();
-        }
-    }
-}
 
 
